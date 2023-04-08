35c0fa8e2b8d4ff17dc9a0bbc5f29b3f934480be
jobayercse71 <rahmanjobayer71@gmail.com>
2023-04-08 14:51:03 +0600

    custom error handler updated

--- src/common/middleware/errorHandler/errorHandler.ts
+++ src/common/middleware/errorHandler/errorHandler.ts
@@ -1,19 +1,48 @@
 import { NextFunction, Request, Response } from "express";
+import CustomError from "../../utils/error/customError";
 
-export const errorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // Handle error
-  console.error(err);
+interface IcustomError {
+  success: boolean;
+  message: string;
+  type: string;
+  status?: number;
+}
 
-  // Set response body
-  res.status(err.status || 500).json({
-    success: false,
-    message: err.message,
-    type: err.type,
-    status: err.status,
-  });
-};
+class ErrorHandler {
+  private customError: IcustomError;
+
+  constructor() {
+    this.customError = {
+      success: false,
+      message: "Something went wrong :( please try again later!!",
+      type: "Internal server error!",
+    };
+  }
+
+  /**
+   * handleErrors
+   */
+  public handleErrors = (
+    err: Error | CustomError,
+    req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    if (err instanceof CustomError) {
+      this.customError.message =
+        err.message || "Something went wrong, please try again later!";
+      this.customError.type = err.type;
+      this.customError.status = err.status;
+    } else {
+      this.customError.message =
+        "Something went wrong, please try again later!";
+      this.customError.type = "Internal Server Error";
+    }
+
+    console.log({ err });
+
+    res.status(this.customError.status || 500).json(this.customError);
+  };
+}
+
+export default ErrorHandler;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, NextFunction, Request, Response } from "express";
-import { errorHandler } from "./common/middleware/errorHandler/errorHandler";
+import ErrorHandler from "./common/middleware/errorHandler/errorHandler";
 import CustomError from "./common/utils/error/customError";
 import { default as AuthRouter } from "./main/router/auth.router";
 
 dotenv.config();
-
 const app: Express = express();
 const port = process.env.PORT || 6000;
 app.use(cors());
@@ -24,9 +23,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 // Add error handling middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  errorHandler(err, req, res, next);
-});
+app.use(new ErrorHandler().handleErrors);
 
 app.listen(port, () => {
   console.log(`⚡️ Server is running at http://localhost:${port}`);
